712af0389526845155e5a644fb10dce385cfe124
Oleg Koltunov <koltunow@gmail.com>
2018-05-29 13:16:15 +0300

    [Add] Initial Commit

---
+++ CoinMarketCap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoinMarketCap
+//
+//  Created by Олег Колтунов on 29/05/2018.
+//  Copyright © 2018 Олег Колтунов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
